1ace002c9f3468432f956531d1e1f0bcb67ec727
Tony Spiro <tspiro@tonyspiro.com>
2015-07-25 22:15:46 -0500

    Delete cosmic.jsx

--- php/astral/assets/jsx/cosmic.jsx
+++
@@ -1,8 +0,0 @@
-
-var HeroUnit = React.createClass({
-	render : function(){
-		return 
-	}
-});
-
-React.render(<HeroUnit url="https://api.cosmicjs.com/v1/astral/objects" />, document.getElementById('hero-unit'));
